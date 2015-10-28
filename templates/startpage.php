48dbeb042fc71f9726e153fe37054c5a1dc319be
jomla97 <johannes.nyman97@hotmail.com>
2015-10-28 01:07:46 +0100

    designat containern för alla varor

--- templates/startpage.php
+++ templates/startpage.php
@@ -5,7 +5,114 @@
 	</head>
 	<body>
 		<main>
-			hej
+			<div class="product-wrapper">
+				<img src="res/vara.jpg">
+				<h2>Namn på vara</h2>
+				<p>Jmf.pris 49,95 kr/kg</p>
+				<div class="info">
+					<div class="info-inner">
+						<p class="item-price">5,99 kr</p>
+						<p>120g</p>
+					</div>
+					<div class="buy">
+						<form name="purchase-item">
+							<input class="amount" type="number" name="amount">
+							<p>st</p>
+							<input class="buy-button" type="submit" value="Köp">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="product-wrapper">
+				<img src="res/vara.jpg">
+				<h2>Namn på vara</h2>
+				<p>Jmf.pris 49,95 kr/kg</p>
+				<div class="info">
+					<div class="info-inner">
+						<p class="item-price">5,99 kr</p>
+						<p>120g</p>
+					</div>
+					<div class="buy">
+						<form name="purchase-item">
+							<input class="amount" type="number" name="amount">
+							<p>st</p>
+							<input class="buy-button" type="submit" value="Köp">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="product-wrapper">
+				<img src="res/vara.jpg">
+				<h2>Namn på vara</h2>
+				<p>Jmf.pris 49,95 kr/kg</p>
+				<div class="info">
+					<div class="info-inner">
+						<p class="item-price">5,99 kr</p>
+						<p>120g</p>
+					</div>
+					<div class="buy">
+						<form name="purchase-item">
+							<input class="amount" type="number" name="amount">
+							<p>st</p>
+							<input class="buy-button" type="submit" value="Köp">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="product-wrapper">
+				<img src="res/vara.jpg">
+				<h2>Namn på vara</h2>
+				<p>Jmf.pris 49,95 kr/kg</p>
+				<div class="info">
+					<div class="info-inner">
+						<p class="item-price">5,99 kr</p>
+						<p>120g</p>
+					</div>
+					<div class="buy">
+						<form name="purchase-item">
+							<input class="amount" type="number" name="amount">
+							<p>st</p>
+							<input class="buy-button" type="submit" value="Köp">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="product-wrapper">
+				<img src="res/vara.jpg">
+				<h2>Namn på vara</h2>
+				<p>Jmf.pris 49,95 kr/kg</p>
+				<div class="info">
+					<div class="info-inner">
+						<p class="item-price">5,99 kr</p>
+						<p>120g</p>
+					</div>
+					<div class="buy">
+						<form name="purchase-item">
+							<input class="amount" type="number" name="amount">
+							<p>st</p>
+							<input class="buy-button" type="submit" value="Köp">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="product-wrapper">
+				<img src="res/vara.jpg">
+				<h2>Namn på vara</h2>
+				<p>Jmf.pris 49,95 kr/kg</p>
+				<div class="info">
+					<div class="info-inner">
+						<p class="item-price">5,99 kr</p>
+						<p>120g</p>
+					</div>
+					<div class="buy">
+						<form name="purchase-item">
+							<input class="amount" type="number" name="amount">
+							<p>st</p>
+							<input class="buy-button" type="submit" value="Köp">
+						</form>
+					</div>
+				</div>
+			</div>
 		</main>
 	</body>
 </html>
